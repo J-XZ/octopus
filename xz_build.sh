3361b9733ff65b47f0630d9a3923b33b319eb7b6
root <root@localhost>
2024-04-27 02:25:42 -0600

    1

---
+++ xz_build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y libfuse-dev libcrypto++-dev libboost-all-dev mpi-default-dev g++ cmake default-jdk g++ librdmacm-dev
+sudo apt autoremove -y
+
+if grep -q "^JAVA_HOME=" /etc/environment; then
+    echo "JAVA_HOME is already set in /etc/environment"
+else
+    echo "JAVA_HOME=/usr/lib/jvm/default-java" | sudo tee -a /etc/environment >/dev/null
+    echo "JAVA_HOME added to /etc/environment"
+fi
+
+source /etc/environment
+
+rm -rf build
+mkdir -p build
+cd build
+cmake ..
+make -j
+cd ..
